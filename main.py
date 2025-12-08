af37bc877e297a20a0eba3c21b982f42039f0158
Rebecca <rebeccahamid00@gmail.com>
2025-12-08 03:30:28 +0000

    added lines 14-25

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ class ChronicKidneyDisApp(wx.Frame):
         super().__init__(parent, title=title, size=(855, 555))
         self.SetMinSize((780, 500))
         panel = wx.Panel(self)
+        
+         # Centered big button
+        button = wx.Button(panel, label="Open Chronic Kidney Disease Dataset")
+        font = button.GetFont()
+        font.PointSize += 2
+        button.SetFont(font)
+        button.Bind(wx.EVT_BUTTON, self.on_open)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.AddStretchSpacer(1)
+        sizer.Add(button, 0, wx.ALIGN_CENTER | wx.ALL, 10)
+        sizer.AddStretchSpacer(2)  # more whitespace below
+        panel.SetSizer(sizer)
